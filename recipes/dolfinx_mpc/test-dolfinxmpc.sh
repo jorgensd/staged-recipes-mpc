f3d29e8f6af253df0b2078f20f8c21395ab935fb
Jørgen S. Dokken <dokken@simula.no>
2022-11-09 12:39:22 +0100

    Try adding conda feedstock

---
+++ recipes/dolfinx_mpc/test-dolfinxmpc.sh
@@ -0,0 +1,17 @@
+set -ex
+
+pip check
+
+# disable clang availability check
+if [[ "$target_platform" == "osx-64" ]]; then
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+export OMPI_MCA_rmaps_base_oversubscribe=1
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+
+cd python/demos
+pytest -vs -k stokes demo_stokes.py
